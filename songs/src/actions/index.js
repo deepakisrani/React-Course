63d55592f7bd963ea89d26b0c65da488eecc25f6
Deepak Israni <deepakmisrani@gmail.com>
2020-05-09 14:32:22 +0530

    Creation of ActionCreator

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,1 +1,11 @@
 // This file is named index because of how webpack behaves, if you were to import this file, you just need to mention the name of the folder
+
+// ActionCreator
+// Named Export
+export const selectSong = (song) => {
+    // Action has to have a type, payload is optional
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    };
+};
