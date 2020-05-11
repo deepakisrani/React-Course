423857c47a5f327b7cd5c532168542a7cccb36dd
Deepak Israni <deepakmisrani@gmail.com>
2020-05-11 23:16:49 +0530

    Using Redux-Thunk as middleware to handle async calls in ActionCreators

--- blog/README.md
+++ blog/README.md
@@ -1,3 +1,7 @@
 # blog
 
 Project to learn about working with async actions with Redux Thunk.
+Topics covered:
+* Working of Redux-Thunk
+* Initializing middleware with store
+* Making async ActionCreators
---
+++ blog/src/actions/index.js
@@ -0,0 +1,22 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// ActionCreators can't be async, when you make it async when you await you do not return a plain JS Object
+export const fetchPosts = () => {
+    // Redux-Thunk calls the returned function with dispatch and getState (from redux)
+    // Because of this, we can manually dispatch our action (after our request is complete)
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: "FETCH_POSTS", payload: response });
+    };
+};
+
+/*
+Can be refactored to: (but left as is for the comments and future reference)
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({ type: "FETCH_POSTS", payload: response });
+};
+
+*/
