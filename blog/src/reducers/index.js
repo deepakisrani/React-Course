35515d3d923663213cb105baef0b824fe1621c10
Deepak Israni <deepakmisrani@gmail.com>
2020-05-15 00:57:35 +0530

    Add rules for reducers.

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+
+/*
+    Rules of Reducers:
+    1. Must return any value other than undefined
+    2. Produces new state using only the previous state and the action
+    3. Reducers must be pure (Should not reach out of itself to decide return value [eg: make an api call])
+    4. Must not mutate input state
+*/
 
 export default combineReducers({
-    toDo: () => 'TODO'
+    posts: postsReducer
 });
