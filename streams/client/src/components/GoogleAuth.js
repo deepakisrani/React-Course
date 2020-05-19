5156a0713f3269f0f1199ff0053d2de44efdcce5
Deepak Israni <deepakmisrani@gmail.com>
2020-05-19 23:57:00 +0530

    Check logged in state through google oauth

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+
+    state = { isSignedIn: null };
     
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
             });
         });
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null ) {
+            return <div>Unsure if we are signed in.</div>;
+        }
+        else if (this.state.isSignedIn) {
+            return <div>Signed in.</div>;
+        }
+        else {
+            return <div>Not signed in.</div>;
+        }
+    }
+
     render() {
-        return <div className="item">Google Auth</div>;
+        return <div className="item">{this.renderAuthButton()}</div>;
     }
 }
 
