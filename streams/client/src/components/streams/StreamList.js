7c45210cde77aacf32cc5178a8eba7a27ceac08a
Deepak Israni <deepakmisrani@gmail.com>
2020-06-05 23:28:51 +0530

    Map streams to users and display edit and delete buttons when the creator is logged in.

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -8,15 +8,44 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            );
+        }
+    }
+
+    renderList() {
+        return this.props.streams.map(stream => (
+            <div className="item" key={stream.id}>
+                {this.renderAdmin(stream)}
+                <i className="large middle aligned icon camera" />
+                <div className="content">
+                    <div className="header">{stream.title}</div>
+                    <div className="description">{stream.description}</div>
+                </div>
+            </div>
+        ));
+    }
+
     render() {
-        console.log(this.props.streams);
-        return <div>StreamList</div>;
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+            </div>
+        );
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        streams: state.streams
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
     };
 }
 
