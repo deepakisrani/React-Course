fd1b51d88f0d6f31de9346ffdf787a2b3d07c620
Deepak Israni <deepakmisrani@gmail.com>
2020-06-14 19:53:42 +0530

    Implement StreamEdit

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -5,9 +5,16 @@ import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
 
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
+    }
+
     render() {
         return (
-            <StreamForm onSubmit={this.props.createStream} />
+            <div>
+                <h3>Create a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} />
+            </div>
         );
     }
 }
--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component  {
 
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        const streamId = this.props.stream.id;
+        this.props.editStream(streamId, formValues);
     }
 
     render() {
@@ -15,10 +21,12 @@ class StreamEdit extends React.Component  {
             return <div>Loading...</div>;
         }
 
+        // initialValues is a special prop of redux-form, if you pass that it autofills inputs with values you passed provided they match
+        // the key value pair with the name provided for the Field component
         return (
             <div>
-                <div>{stream.title}</div><br/>
-                <div>{stream.description}</div>
+                <h3>Edit a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} initialValues={this.props.stream} />
             </div>
         );
     }
@@ -30,4 +38,4 @@ const mapStateToProps = (state, ownProps) => {
     }
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
