d9653487e9e3fcdf54236b075c9e26a429d84b4e
Deepak Israni <deepakmisrani@gmail.com>
2020-06-18 22:17:47 +0530

    Finish up basic CRUD functionality of streams

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
 import history from '../history';
@@ -14,17 +14,20 @@ class App extends React.Component {
 
     // If you have a component outside BrowserRouter, it will be shown 100% of the time (provided it doesn't use any Router related components)
     // BrowserRouter ignores the history prop. Plain Router can use custom history
+    // Switch shows only one of the route that matches, in order to avoid clashes of /new and /:id
     render() {
         return (
             <div className="ui container">
                 <Router history={history}>
                     <div>
                         <Header />
-                        <Route path="/" exact component={StreamList} />
-                        <Route path="/streams/new" exact component={StreamCreate} />
-                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                        <Route path="/streams/show" exact component={StreamShow} />
+                        <Switch>
+                            <Route path="/" exact component={StreamList} />
+                            <Route path="/streams/new" exact component={StreamCreate} />
+                            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                            <Route path="/streams/:id" exact component={StreamShow} />
+                        </Switch>
                     </div>
                 </Router>
             </div>
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -23,14 +23,18 @@ class StreamList extends React.Component {
 
     renderList() {
         return this.props.streams.map(stream => (
-            <div className="item" key={stream.id}>
-                {this.renderAdmin(stream)}
-                <i className="large middle aligned icon camera" />
-                <div className="content">
-                    <div className="header">{stream.title}</div>
-                    <div className="description">{stream.description}</div>
+            
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        <Link to={`/streams/${stream.id}`} className="header" >
+                            {stream.title}
+                        </Link>
+                        <div className="description">{stream.description}</div>
+                    </div>
                 </div>
-            </div>
+            
         ));
     }
 
