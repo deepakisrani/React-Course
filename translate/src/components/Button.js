53d27288a85bde9f40df47b4237aebc18faf1736
Deepak Israni <deepakmisrani@gmail.com>
2020-08-02 00:18:08 +0530

    Consume context value

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,10 +3,12 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-    static contextType = LanguageContext; // context object hooked up to a class component
+    static contextType = LanguageContext; // context object hooked up to a class component, naming has to be exactly like this
+    // Context value can be accessed via this.context
+
     render() {
-        console.log(this.context);
-        return <button className="ui button primary">Submit</button>
+        const text = this.context === 'english' ? 'Submit' : "Voorleggen";
+        return <button className="ui button primary">{text}</button>
     }
 }
 
