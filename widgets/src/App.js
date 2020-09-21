2caf3c8139ab05454edc4fe65539c76bc7437596
Deepak Israni <deepakmisrani@gmail.com>
2020-09-21 10:29:48 +0530

    Implement basic functionality of dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -18,11 +19,37 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+];
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div className="ui container">
             {false && (<Accordion items={items} />)}
-            {true && (<Search />)}
+            {false && (<Search />)}
+            {true && 
+                (
+                    <Dropdown
+                        selected={selected}
+                        onSelectedChange={setSelected}
+                        options={options} 
+                    />
+                )
+            }
         </div>
     )
 }
