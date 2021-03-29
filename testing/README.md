717ccd5cc373b108aaef78edb656a8856ddc28d0
Deepak Israni <deepakmisrani@gmail.com>
2021-03-29 23:52:30 +0530

    Add tests for CommentBox and hook up redux to test components

--- testing/README.md
+++ testing/README.md
@@ -1,5 +1,11 @@
 # testing
 
 This app contains topics related to writing tests for a react application. Topics covered:
-1. expect()
-2. Enzyme and Enzyme Adapter
+* expect()
+* Enzyme and Enzyme Adapter
+* beforeEach
+* afterEach
+* Simulating change events
+* Retrieving prop values in Enzyme
+* describe statement
+* Add redux to test components
--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const CommentBox = () => {
-    return <div>Comment Box</div>;
-};
+import * as actions from 'actions';
 
-export default CommentBox;
+class CommentBox extends Component {
+    state = { comment: '' };
+
+    handleChange = (event) => {
+        this.setState({ comment: event.target.value });
+        // setState re-renders the component asynchronously
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.saveComment(this.state.comment);
+        this.setState({ comment: '' });
+    };
+
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea value={this.state.comment} onChange={this.handleChange} />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default connect(null, actions)(CommentBox);
